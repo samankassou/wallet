015b23d33947908a57c9460b45c7c06a0ec08cd8
samankassou <samankassoufoulla@gmail.com>
2023-02-15 02:24:50 +0100

    Add nuxt auth module

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,43 +4,80 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'wallet',
+    title: "wallet",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      {
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap",
+      },
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
+  buildModules: [],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     // https://go.nuxtjs.dev/bootstrap
-    'bootstrap-vue/nuxt',
+    "bootstrap-vue/nuxt",
+    "@nuxtjs/axios",
+    "@nuxtjs/auth-next",
   ],
+  auth: {
+    strategies: {
+      cookie: {
+        endpoints: {
+          csrf: {
+            url: "/sanctum/csrf-cookie",
+          },
+          login: {
+            url: "/login",
+          },
+          logout: {
+            url: "/logout",
+          },
+          user: {
+            url: "/user",
+          },
+        },
+        user: {
+          property: "data",
+        },
+      },
+    },
+
+    redirect: {
+      login: "/login",
+      logout: "/login",
+      home: "/",
+    },
+
+    plugins: ["~/plugins/axios"],
+  },
+  axios: {
+    baseURL: "http://localhost:8000/api/v1",
+    credentials: true,
+  },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  }
-}
+  build: {},
+};
