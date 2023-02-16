d5e1637dff8b6abfd2615d0e2713f1b49973159c
samankassou <samankassoufoulla@gmail.com>
2023-02-16 11:45:44 +0100

    display users

---
+++ pages/users/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <h1 class="mt-4">Liste des utilisateurs</h1>
+    <b-table striped hover :items="users" :fields="fields"></b-table>
+  </div>
+</template>
+<script>
+export default {
+  middleware: "auth",
+  auth: "guest",
+  head() {
+    return {
+      title: "Utilisateurs — Wallet",
+    };
+  },
+
+  data: () => ({
+    fields: [
+      {
+        key: "lastname",
+        sortable: true,
+        label: "Prénom(s)",
+      },
+      {
+        key: "firstname",
+        sortable: false,
+        label: "Nom(s)",
+      },
+      {
+        key: "email",
+        label: "Adresse mail",
+        sortable: true,
+      },
+      {
+        key: "role",
+        label: "Rôle",
+        sortable: true,
+      },
+    ],
+    users: [],
+  }),
+  methods: {
+    rowClass(item) {
+      if (!item || !item.status) return;
+      if (item.status) return "table-success";
+    },
+  },
+  async fetch() {
+    const response = await this.$axios.$get(
+      "http://localhost:8000/api/v1/users"
+    );
+
+    this.users = response.data;
+  },
+};
+</script>
