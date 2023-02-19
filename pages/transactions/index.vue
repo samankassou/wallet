cbe359cc6cfeb86637636c176fbf4579242e7ef5
samankassou <samankassoufoulla@gmail.com>
2023-02-19 08:58:09 +0100

    Toggle user status

--- pages/transactions/index.vue
+++ pages/transactions/index.vue
@@ -3,21 +3,40 @@
     <h1 class="mt-4">Historique de vos opérations</h1>
     <b-row class="mb-4">
       <b-col>
-        <b-btn variant="outline-primary" to="/transactions/expense">
+        <b-btn variant="outline-info" to="/transactions/expense">
           Enregistrer une dépense
         </b-btn>
-        <b-btn to="/transactions/income"> Déclarer un revenu </b-btn>
+        <b-btn variant="info" to="/transactions/income">
+          Déclarer un revenu
+        </b-btn>
       </b-col>
     </b-row>
     <b-table
+      id="transactions"
       striped
       hover
       :items="transactions"
       :fields="formattedFileds"
       :busy.sync="$fetchState.pending"
       :tbody-tr-class="rowClass"
+      :per-page="pagination?.per_page"
+      :total-rows="pagination.total"
     >
+      <template #table-busy>
+        <div class="text-center text-danger my-2">
+          <b-spinner class="align-middle"></b-spinner>
+          <strong>Loading...</strong>
+        </div>
+      </template>
     </b-table>
+    <b-pagination
+      :value="pagination.current_page"
+      :total-rows="pagination.total"
+      :per-page="pagination?.per_page"
+      aria-controls="transactions"
+      variant="info"
+      @change="handlePageChange"
+    ></b-pagination>
   </div>
 </template>
 <script>
@@ -29,6 +48,9 @@ export default {
     };
   },
   data: () => ({
+    pagination: {
+      current_page: 1,
+    },
     fields: [
       {
         key: "type",
@@ -59,8 +81,15 @@ export default {
       }
       return this.fields;
     },
+    rows() {
+      return this.transactions.length;
+    },
   },
   methods: {
+    handlePageChange(value) {
+      this.currentPage = value;
+      this.$fetch();
+    },
     rowClass(item) {
       if (!item) return;
       if (item.type === "expense") return "table-danger";
@@ -68,9 +97,12 @@ export default {
     },
   },
   async fetch() {
-    const response = await this.$axios.$get("/api/transactions");
+    const response = await this.$axios.$get(
+      `/api/transactions?page=${this.pagination.current_page}`
+    );
 
     this.transactions = response.data;
+    this.pagination = response?.meta;
   },
 };
 </script>
--- pages/users/index.vue
+++ pages/users/index.vue
@@ -10,6 +10,21 @@
       :tbody-tr-class="rowClass"
       :actions="actions"
     >
+      <template #cell(Statut)="row">
+        <b-button
+          :variant="row.item.status ? 'danger' : 'success'"
+          size="sm"
+          class="mr-2"
+          @click="toggleStatus(row.item.id)"
+          >{{ row.item.status ? "Désactiver" : "Activer" }}
+        </b-button>
+      </template>
+      <template #table-busy>
+        <div class="text-center text-danger my-2">
+          <b-spinner class="align-middle"></b-spinner>
+          <strong>Chargement...</strong>
+        </div>
+      </template>
     </b-table>
     <b-col sm="7" md="6" class="my-1">
       <b-pagination
@@ -60,6 +75,7 @@ export default {
         label: "Adresse mail",
         sortable: true,
       },
+      "Statut",
       {
         key: "role",
         label: "Rôle",
@@ -69,12 +85,35 @@ export default {
     users: [],
   }),
   methods: {
+    makeToast() {
+      this.$bvToast.toast(`Mis à jour avec succès`, {
+        title: "Wallet",
+        autoHideDelay: 5000,
+        variant: "success",
+      });
+    },
     rowClass(item) {
       if (!item || !item.status) return "table-danger";
       if (item.status) return;
     },
     editItem(item, index, target) {
       console.log(item);
+    },
+    toggleStatus(id) {
+      this.$axios
+        .$patch(`/api/users/toggle-status/${id}`)
+        .then((res) => {
+          this.makeToast();
+          this.$fetch();
+          //this.reset();
+          //this.$router.push("/transactions");
+        })
+        .catch((error) => {
+          console.log(error);
+          //if (error.response && error.response.status !== 422) throw error;
+
+          //this.errors = Object.values(error?.response?.data?.errors).flat();
+        });
     },
   },
   async fetch() {
